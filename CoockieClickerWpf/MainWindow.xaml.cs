7a3475b6f7c2416aa46357eb8af6e0d73f518a81
Michiel Van Eester <michiel.vaneester@immalle.be>
2016-10-20 11:51:30 +0200

    Add project files.

---
+++ CoockieClickerWpf/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CoockieClickerWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int getal = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Coockie_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Coockie.Fill = new SolidColorBrush(Colors.SaddleBrown);
+            Coockie.StrokeThickness = 4;
+            Coockie.Stroke = new SolidColorBrush(Colors.Black);
+        }
+
+        private void Coockie_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Coockie.Fill = new SolidColorBrush(Colors.Gray);
+            Coockie.StrokeThickness = 2;
+            Coockie.Stroke = new SolidColorBrush(Colors.Black);
+        }
+
+        private void Ellipse_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Coockie.Fill = new SolidColorBrush(Colors.SaddleBrown);
+            Coockie.StrokeThickness = 4;
+            Coockie.Stroke = new SolidColorBrush(Colors.Black);
+        }
+
+        private void Ellipse_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Coockie.Fill = new SolidColorBrush(Colors.Gray);
+            Coockie.StrokeThickness = 2;
+            Coockie.Stroke = new SolidColorBrush(Colors.Black);
+        }
+
+        private void Ellipse_MouseEnter_1(object sender, MouseEventArgs e)
+        {
+            Coockie.Fill = new SolidColorBrush(Colors.SaddleBrown);
+            Coockie.StrokeThickness = 4;
+            Coockie.Stroke = new SolidColorBrush(Colors.Black);
+        }
+
+        private void Ellipse_MouseLeave_1(object sender, MouseEventArgs e)
+        {
+            Coockie.Fill = new SolidColorBrush(Colors.Gray);
+            Coockie.StrokeThickness = 2;
+            Coockie.Stroke = new SolidColorBrush(Colors.Black);
+        }
+
+        private void Ellipse_MouseEnter_2(object sender, MouseEventArgs e)
+        {
+            Coockie.Fill = new SolidColorBrush(Colors.SaddleBrown);
+            Coockie.StrokeThickness = 4;
+            Coockie.Stroke = new SolidColorBrush(Colors.Black);
+        }
+
+        private void Ellipse_MouseLeave_2(object sender, MouseEventArgs e)
+        {
+            Coockie.Fill = new SolidColorBrush(Colors.Gray);
+            Coockie.StrokeThickness = 2;
+            Coockie.Stroke = new SolidColorBrush(Colors.Black);
+        }
+
+        private void Coockie_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            getal = getal + 1;
+            teller.Text = String.Format("{0}", getal);  
+        }
+
+        private void Ellipse_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            getal = getal + 1;
+            teller.Text = String.Format("{0}", getal);
+        }
+
+        private void Ellipse_MouseLeftButtonDown_1(object sender, MouseButtonEventArgs e)
+        {
+            getal = getal + 1;
+            teller.Text = String.Format("{0}", getal);
+        }
+
+        private void Ellipse_MouseLeftButtonDown_2(object sender, MouseButtonEventArgs e)
+        {
+            if (getal < 500)
+            {
+                getal = getal + 1;
+                teller.Text = String.Format("{0}", getal);
+            }
+
+            if (getal >= 500)
+            {
+                getal = getal + 1;
+                teller.Text = String.Format("{0}", getal);
+            }
+        }
+    }
+}
